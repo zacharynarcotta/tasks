6262b44a7952f4f6a9a1de5d7580cef2de1a3636
zacharynarcotta <zachary.narcotta@maine.edu>
2026-01-27 19:40:47 -0500

    Add name onto site

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App(): React.JSX.Element {
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
+                Modified by Zachary Narcotta
             </p>
         </div>
     );
