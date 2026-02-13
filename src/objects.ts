83ef1869621aedf9ad8b11ee80514bdb983c0ae3
zacharynarcotta <zachary.narcotta@maine.edu>
2026-02-13 15:55:31 -0500

    Implements isValid function

--- src/objects.ts
+++ src/objects.ts
@@ -42,7 +42,9 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    return question.type === "short_answer_question" ?
+            true
+        :   question.options.includes(answer);
 }
 
 /**
