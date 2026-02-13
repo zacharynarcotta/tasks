f3968279a0144c3b95d473693d10457146175655
zacharynarcotta <zachary.narcotta@maine.edu>
2026-02-13 16:10:57 -0500

    Implements publishQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -101,7 +101,7 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return { ...question, published: !question.published };
 }
 
 /**
