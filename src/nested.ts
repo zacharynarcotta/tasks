f5fd915c0fe02bf3534e92cc3ac1fc21a50ec754
zacharynarcotta <zachary.narcotta@maine.edu>
2026-02-21 14:22:38 -0500

    Implements publishAll function

--- src/nested.ts
+++ src/nested.ts
@@ -84,7 +84,9 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * Hint: as usual, do not modify the input questions array
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return questions.map(
+        (question: Question): Question => ({ ...question, published: true }),
+    );
 }
 
 /***
